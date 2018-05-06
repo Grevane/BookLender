88bd15ea8fed289362f9de96e90eb69c659b6676
Grevane <shhh.i.be.readin@gmail.com>
2018-05-06 14:44:45 -0500

    Add styles component

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './components/styles/styles.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <Text>Text Change</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
